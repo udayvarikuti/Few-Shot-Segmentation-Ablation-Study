fa10c7be6b4c2d144c801c86fa9094f95465e7d3
pmkamath <pkamath@ucsd.edu>
2022-05-30 01:55:30 -0700

    Added deeplab v3 pretrained model and script for fewshot import
    Added deeplab v3 pretrained model and script for fewshot import

---
+++ panet/src/models/deeplabv3.py
@@ -0,0 +1,27 @@
+"""
+Encoder for few shot segmentation (VGG16)
+"""
+
+import torch
+import torch.nn as nn
+
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels=3, pretrained_path=None):
+        super().__init__()
+        self.features = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet101', pretrained=True)
+        self.features.aux_classifier=Identity()
+        #print(type(self.features))
+
+    def forward(self, x):
+        y=self.features(x)['out']
+        #print(type(y))
+        #print(y)
+        return y
